455f089ec0b5048268c200d727bfb807570b10c9
passion-logan <wangdadamailbox@163.com>
2019-12-06 18:34:27 +0800

    添加修改

--- eureka-consumer-ribbon-hystrix/src/main/java/com/cody/controller/DcController.java
+++ eureka-consumer-ribbon-hystrix/src/main/java/com/cody/controller/DcController.java
@@ -1,6 +1,9 @@
 package com.cody.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 应用模块名称<p>
@@ -12,4 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class DcController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("consumer")
+    public String dc() {
+        return restTemplate.getForObject("http://eureka-client/dc", String.class);
+    }
 }
