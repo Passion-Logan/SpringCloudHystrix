4489ca79067c45e6c4ae8bf65b18a2f7352e4357
passion-logan <wangdadamailbox@163.com>
2019-12-06 18:37:52 +0800

    添加修改

---
+++ eureka-consumer-ribbon-hystrix/src/main/java/com/cody/service/ConsumerService.java
@@ -0,0 +1,32 @@
+package com.cody.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCollapser;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.utils.FallbackMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ * Copyright: Copyright (C) 2019 XXX, Inc. All rights reserved. <p>
+ *
+ * @author WQL
+ * @since 2019年12月6日 0006 18:35
+ */
+@Service
+public class ConsumerService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "fallback")
+    public String consumer() {
+        return restTemplate.getForObject("http://eureka-client/dc", String.class);
+    }
+
+    public String fallback() {
+        return "fallback";
+    }
+}
