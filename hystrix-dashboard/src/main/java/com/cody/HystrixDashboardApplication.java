f6419505b8825912648c5ae0ae9309ce95269f02
passion-logan <wangdadamailbox@163.com>
2019-12-11 16:26:22 +0800

    应用主类修改

--- hystrix-dashboard/src/main/java/com/cody/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/cody/HystrixDashboardApplication.java
@@ -2,9 +2,14 @@ package com.cody;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-
-@SpringBootApplication
+/**
+ * 配置@EnableHystrixDashboard 开启Hystrix Dashboard功能
+ */
+@EnableHystrixDashboard
+@SpringCloudApplication
 public class HystrixDashboardApplication {
 
     public static void main(String[] args) {
