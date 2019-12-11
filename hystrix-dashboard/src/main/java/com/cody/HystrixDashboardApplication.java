055834a37faab85b059507d0fade022cea833c9d
passion-logan <wangdadamailbox@163.com>
2019-12-11 16:22:16 +0800

    修改依赖

--- hystrix-dashboard/src/main/java/com/cody/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/cody/HystrixDashboardApplication.java
@@ -3,6 +3,7 @@ package com.cody;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class HystrixDashboardApplication {
 
