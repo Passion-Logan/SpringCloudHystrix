93f279705ba69cdf60994d61921625103e3b837b
passion-logan <wangdadamailbox@163.com>
2019-12-11 17:25:54 +0800

    修改配置

--- turbine/src/main/java/com/cody/TurbineApplication.java
+++ turbine/src/main/java/com/cody/TurbineApplication.java
@@ -1,9 +1,17 @@
 package com.cody;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@EnableTurbine
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
 public class TurbineApplication {
 
     public static void main(String[] args) {
