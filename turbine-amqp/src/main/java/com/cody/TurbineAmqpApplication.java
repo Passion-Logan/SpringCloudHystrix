5db2dddf57b49bc8f9b4d3373c0d3c7582f03403
passion-logan <wangdadamailbox@163.com>
2019-12-11 17:35:40 +0800

    SpringCloudTurbine-amqp 通过消息代理收集聚合

---
+++ turbine-amqp/src/main/java/com/cody/TurbineAmqpApplication.java
@@ -0,0 +1,13 @@
+package com.cody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbineAmqpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineAmqpApplication.class, args);
+    }
+
+}
